8e00cec3cc8e2483791ebc3b1fc17df02721197f
Krishna Kumar Mandal <mandalkrishna739@gmail.com>
2024-07-27 08:28:55 +0545

    Defaut Pipes and custom pipes

--- src/app/pipes/pipes.component.ts
+++ src/app/pipes/pipes.component.ts
@@ -1,14 +1,29 @@
-import { DatePipe, UpperCasePipe } from '@angular/common';
+import { AsyncPipe, DatePipe, JsonPipe, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { interval, map, Observable } from 'rxjs';
+import { NaPipe } from '../customPipe/na.pipe';
 
 @Component({
   selector: 'app-pipes',
   standalone: true,
-  imports: [DatePipe, UpperCasePipe],
+  imports: [DatePipe, UpperCasePipe, JsonPipe, AsyncPipe, NaPipe],
   templateUrl: './pipes.component.html',
   styleUrl: './pipes.component.css'
 })
 export class PipesComponent {
   birthday = "2002-09-01"
-  currentTime = new Date();
+  currentDate = new Date();
+
+  // Its show time
+  currentTime: Observable<Date> = new Observable<Date>;
+  constructor() {
+    this.currentTime = interval(1000).pipe(map(() => new Date()))
+  }
+
+  student = {
+    name: 'krishna',
+    city: 'ktm',
+    empId: 123,
+    state: ''
+  }
 }
